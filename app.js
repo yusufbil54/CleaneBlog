05bf9a045cee586565e04ad2d1110f282e050a19
Yusuf Bil <104075393+yusufbil54@users.noreply.github.com>
2023-02-07 16:50:36 +0300

    EJS Uploeded

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const express = require("express");
+const ejs=require('ejs');
+
 const app = express();
-app.get("/", (req, res) => {
-  const blog = {
-    id: 1,
-    title: "Blog Title",
-    description: "Blog Description",
-  };
-  res.send(blog);
+
+const path=require('path');
+
+//TEMPLATE ENGINE
+app.set("view engine","ejs");
+
+//MIDDLEWEARS
+app.use(express.static('public'));
+
+//ROUTES
+app.get("/",(req, res) => {
+  res.render('index');
 });
+app.get("/about",(req, res) => {
+  res.render('about');
+});
+app.get("/add_post",(req, res) => {
+  res.render('add_post');
+});
+app.get("/post",(req, res) => {
+  res.render('post');
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} numarali porta baglandi`);
