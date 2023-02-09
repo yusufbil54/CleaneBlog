267fc5012af6b7532817934670edb80bfe0237a5
Yusuf Bil <104075393+yusufbil54@users.noreply.github.com>
2023-02-09 16:45:44 +0300

    MongoDB Updated

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
-const express = require("express");
-const ejs=require('ejs');
-
+const express = require('express');
+const ejs = require('ejs');
+const Blog = require('./models/Blog');
 const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
 
-const path=require('path');
+//Connection DB
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://localhost/cleanblog-test-db');
 
 //TEMPLATE ENGINE
-app.set("view engine","ejs");
+app.set('view engine', 'ejs');
 
 //MIDDLEWEARS
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //ROUTES
-app.get("/",(req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const blog = await Blog.find({});
+  res.render('index', {
+    blog,
+  });
 });
-app.get("/about",(req, res) => {
+app.get('/about', (req, res) => {
   res.render('about');
 });
-app.get("/add_post",(req, res) => {
+app.get('/add_post', (req, res) => {
   res.render('add_post');
 });
-app.get("/post",(req, res) => {
+app.get('/post', (req, res) => {
   res.render('post');
+});
+app.post('/blog', async (req, res) => {
+  await Blog.create(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
